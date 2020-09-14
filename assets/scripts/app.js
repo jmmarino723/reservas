b12746d762b8ab08dd359b1a43ab90d66019714b
hedav <hd.sanchezpaz@gmail.com>
2020-09-14 16:07:35 -0500

    Update app.js
    Branch de master sin los cambios hechos despues

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,1 @@
-const modal = document.getElementById('add-modal');
-const button = document.querySelector('header button');
-const backdrop = document.getElementById('backdrop');
-const btnCancelar = modal.querySelector('.btn--passive');
 
-function alternarFondo(){
-    backdrop.classList.toggle('visible');
-}
-
-function mostrarModal(){
-    modal.classList.toggle('visible');
-    alternarFondo();
-}
-
-
-button.addEventListener('click', mostrarModal)
-backdrop.addEventListener('click', mostrarModal);
-btnCancelar.addEventListener('click', mostrarModal);
